f59d4f9571ad7d9a91e080b18ccb85e5e4e3bd11
chandan-16 <chandangautam420.cg@gmail.com>
2026-06-28 18:18:30 +0530

    Added the tailwindcss and a flowbite which is tailwind library to boost up with the styling part

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,5 +1,9 @@
 export const MovieList = () => {
   return (
-    <main>MovieList</main>
+    <main>
+       <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </main>
   )
 }
